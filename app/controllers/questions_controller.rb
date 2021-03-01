c72f5de7481e628ec049e0b98c1848d25eba1b99
Mehdi Seffar <mehdi.seffar@outlook.fr>
2021-03-01 14:22:31 +0100

    exercice 2 done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,14 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    # @answer = params[:answer]
+    if params[:ask] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:ask].chars.include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
